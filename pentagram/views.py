1a0d6ae21d40ce8020fa8221feb14dd02da871f4
BiancaBM <bianca.blebea91@gmail.com>
2016-07-25 13:53:22 +0300

    modified settings

--- pentagram/views.py
+++ pentagram/views.py
@@ -29,7 +29,7 @@ def photos(request):
 @api_view(['GET','POST'])
 def comments(request, id_photo):
     if request.method == "GET":
-        comments = Comment.objects.filter(id_photo)
+        comments = Comment.objects.filter(photo=id_photo)
         serializer = CommentSerializer(comments, many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
     if request.method == "POST":
@@ -56,11 +56,11 @@ def likes(request, id_photo):
         like = Like.objects.filter(photo=id_photo).count()
         return Response(status=status.HTTP_200_OK, data=like)
     if request.method == "POST":
-        phot = Photo.objects.get(id = id_photo)
+        phot = Photo.objects.get(id=id_photo)
         like = Like.objects.filter(photo=phot, user=request.user).count()
         if like == 0:
-            Like.objects.create(photo = phot, user=request.user).save()
+            Like.objects.create(photo=phot, user=request.user).save()
             return Response(status=status.HTTP_200_OK)
         else:
             Like.objects.filter(photo=id_photo, user=request.user).delete()
-            return Response(status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_205_RESET_CONTENT)
